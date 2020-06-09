c8ebd98cb90662d37807c0445f060939c25c0c01
matsuyoshi <sfbgwm30@gmail.com>
2020-06-10 01:22:09 +0900

    Add chapter 1 until 1.2.4

---
+++ ch01/ch01.scm
@@ -0,0 +1,226 @@
+;;;;; 1. Building Abstractions with Procedures
+
+;;;; 1.1 The Elements of Programming
+
+;;; 1.1.1 Expressions
+486 ; 486
+(+ 137 349) ; 486
+(- 1000 334) ; 666
+(* 5 99) ; 495
+(/ 10 5) ; 2
+(+ 2.7 10) ; 12.7
+;; (operator argument)
+;; argument = operand*
+(+ 21 35 12 7) ; 75
+(* 25 4 12) ; 1200
+
+;;; 1.1.2 Naming and Environment
+(define size 2)
+size ; 2
+(* 5 size) ; 10
+
+;;; 1.1.3 Evaluating Combinations
+;; 組み合わせの評価
+;; 1) 組み合わせの部分式の評価
+;; 2) 部分式を部分式の残り値（引数）に適用する
+;; => 一般的評価規則（例外は特殊形式 special form と呼ばれる）
+(* (+ 2 (* 4 6))
+   (+ 3 5 7))
+;; 上の例の場合、4つの組み合わせに対して評価規則が再帰的に適用される
+
+;;; 1.1.4 Compound Procedures
+(define (square x) (* x x))
+;; (define (name parameter) (body))
+(square 21) ; 441
+(square (+ 2 5)) ; 49
+(square (square 3)) ; 81
+(define (sum-of-square x y) (+ (square x) (square y)))
+(sum-of-square 3 4) ; 25
+(define (f a) (sum-of-square (+ a 1) (* a 2)))
+(f 5) ; 136
+
+;;; 1.1.5 The substitution model for procedure application
+;; 複合手続きを引数に適用するには、手続き本体に出てくる仮引数parameterを対応する引数argumentで置換し、それを評価する
+;; 簡単なモデル
+(f 5)
+;; -> (sum-of-square (+ 5 1) (* 5 2))
+;; -> (+ (square 6) (square 10))
+;; -> (+ (* 6 6) (* 10 10))
+;; -> (+ 36 100)
+;; => 136
+;; 「値が必要になるまで被演算子を評価しない」という適用順序の考え方もある（結果は同じ）
+;; 完全に展開してから簡約する = 正規順序評価 normal-order evaluation
+;; 引数を評価してから適用する = 適用順序評価 application-order evaluation （Lispはこっち）
+
+;;; 1.1.6 Conditional Expressions and Predicates
+(define (abs x)
+  (cond ((> x 0) x)
+        ((= x 0) 0)
+        ((< x 0) -x)))
+;; (cond (p1 e1) (p2 e2) ... (pn en))
+(define (abx x)
+  (cond ((< x 0) -x)
+        (else x)))
+;; (if predicate consequent alternative)
+(define (abs x)
+  (if (< x 0)
+      (- x)
+      x))
+;; (and e1 ... en)
+;; (or e1 ... en)
+;; (not e)
+
+;;; 1.1.7 Example of square root by newton's method
+;; 数学の関数とコンピュータの手続きの違い = 手続きは実効的effectiveでなければならない
+;; 関数は「物事の属性について説明するもの」、手続きは「どうやって物事を行うかについて説明するもの」
+(define (improve guess x)
+  (average guess (/ x guess)))
+(define (average x y)
+  (/ (+ x y) 2))
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x) x)))
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+(sqrt 9) ; 3.00009155413138
+(sqrt (+ 100 37)) ; 11.704699917758145
+(sqrt (+ (sqrt 2) (sqrt 3))) ; 1.7739279023207892
+(square (sqrt 1000)) ; 1000.000369924366
+
+;;; 1.1.8 Procedures as black-box abstractions
+;; 手続き分割のポイント
+;; 分割したそれぞれの手続きが特定のタスクを成し遂げていて、それが他の手続きを定義する際にもモジュールとして使用できる
+;; -> 使用するモジュールはブラックボックスとして、「どうするか」は気にせず「何をするか」だけ知っていれば良い
+
+;; 内部定義とブロック構造を用いて sqrt を定義する
+;; 補助手続きを内部化することにより、sqrt に束縛されている x を補助手続きに明示的に渡す必要がない
+;; ref. レキシカルスコープ
+(define (sqrt x)
+  (define (good-enough? guess)
+    (< (abs (- (square guess) x)) 0.001))
+  (define (improve guess) (average guess (/ x guess)))
+  (define (sqrt-iter guess)
+    (if (good-enough? guess)
+        guess
+        (sqrt-iter (improve guess))))
+  (sqrt-iter 1.0 x))
+
+;;;; 1.2 Procedures and the processes they generate
+
+;;; 1.2.1 Linear recursion and iteration
+(define (factorial n)
+  (if (= n 1)
+      1
+      (* n (factorial (- n 1)))))
+;; 線形再帰プロセス
+;; (factorial 6)
+;; (* 6 (factorial 5))
+;; (* 6 (* 5 (factorial 4)))
+;; (* 6 (* 5 (* 4 (factorial 3))))
+;; (* 6 (* 5 (* 4 (* 3 (factorial 2)))))
+;; (* 6 (* 5 (* 4 (* 3 (* 2 (factorial 1))))))
+;; (* 6 (* 5 (* 4 (* 3 (* 2 1)))))
+;; (* 6 (* 5 (* 4 (* 3 2))))
+;; (* 6 (* 5 (* 4 6)))
+;; (* 6 (* 5 24))
+;; (* 6 120)
+;; 720
+;; 展開から縮約
+;; n に対してステップ数が線形に変化し、メモリ消費も再帰呼び出し回数に比例する
+(define (factorial n)
+  (fact-iter 1 1 n))
+(define (fact-iter product counter max-count)
+  (if (> counter max-count)
+      product
+      (fact-iter (* counter product)
+                 (+ counter 1)
+                 max-count)))
+;; 線形反復プロセス
+;; (factorial 6)
+;; (fact-iter 1 1 6)
+;; (fact-iter 1 2 6)
+;; (fact-iter 2 3 6)
+;; (fact-iter 6 4 6)
+;; (fact-iter 24 5 6)
+;; (fact-iter 120 6 6)
+;; (fact-iter 720 7 6)
+;; 720
+;; 状態が限られた数の状態変数に集約される
+;; 再帰手続きで実装されていても固定の空間で実行できる
+
+;;; 1.2.2 Tree recursion
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib (- n 1)) (fib (- n 2))))))
+;; 木構造で表すことができる再帰
+;; 無駄が多い
+;; a, bという整数のペアを使って、反復プロセスとして定式化できる
+;; a <- a + b
+;; b <- a
+(define (fib n)
+  (fib-iter 1 0 n))
+(define (fib-iter a b count)
+  (if (= count 0)
+      b
+      (fib-iter (+ a b) a (- count 1))))
+
+;; 例）両替パターン
+;; 50, 25, 10, 5, 1セントの硬貨が使用できる場合
+(define (count-change amount) (cc amount 5))
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (= kinds-of-coins 0)) 0)
+        (else (+ (cc amount
+                     (- kinds-of-coins 1))
+                 (cc (- amount
+                        (first-denomination
+                         kinds-of-coins))
+                     kinds-of-coins)))))
+;; 使用できる硬貨の種類数が与えられ、1つ目の種類のコインの額面を返す
+(define (first-denomination kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+        ((= kinds-of-coins 2) 5)
+        ((= kinds-of-coins 3) 10)
+        ((= kinds-of-coins 4) 25)
+        ((= kinds-of-coins 5) 50)))
+;; n種類のコインを使って金額aを両替するやり方のパターン数
+;;  1) 1つ目の種類のコイン以外のすべての種類のコインを使って金額aを両替
+;;  2) n種類の硬貨すべてを使って、金額a-dを両替（dは1つ目の種類のコインの額面）
+
+;;; 1.2.3 Orders of growth
+;; 入力が増えるに従ってプロセスが必要とする計算リソースはどの程度になるか
+
+;;; 1.2.4 Exponentation
+;; recursion
+(define (expt b n)
+  (if (= n 0)
+      1
+      (* b (expt b (- n 1)))))
+;; O(n)のステップ数、O(n)の空間を要する
+;; iteration
+(define (expt b n)
+  (expt-iter b n 1))
+(define (expt-iter b counter product)
+  (if (= counter 0)
+      product
+      (expt-iter b (- counter 1) (* b product))))
+;; O(n)のステップ数、O(1)の空間を要する
+;; 指数が2のべき乗である場合、例えばb^8のときは以下のように3回で計算できる
+;; b^2 = b * b
+;; b^4 = b^2 * b^2
+;; b^8 = b^4 * b^4
+;; 偶奇を考慮することでより一般的な定義が可能
+;; n が偶 = b^n = (b^(n/2))^2 => b^n
+;; n が奇 = b^n = b*b^(n-1)   => b^n
+(define (fast-expt b n)
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
+(define (even? n)
+  (= (remainder n 2) 0))
+;; 空間、ステップ数ともに対数的な増加となる
---
+++ ch01/ex01.scm
@@ -0,0 +1,280 @@
+;;; 1.1
+10 ; 10
+(+ 5 3 4) ; 12
+(- 9 1) ; 8
+(/ 6 2) ; 3
+(+ (* 2 4) (- 4 6)) ; 6
+(define a 3)
+(define b (+ a 1))
+(+ a b (* a b)) ; 19
+(= a b) ; #f
+(if (and (> b a) (< b (* a b)))
+    b
+    a) ; 4
+(cond ((= a 4) 6)
+      ((= b 4) (+ 6 7 a))
+      (else 25)) ; 16
+(+ 2 (if (> b a) b a)) ; 6
+(* (cond ((> a b) a)
+         ((< a b) b)
+         (else -1))
+   (+ a 1)) ; 16
+
+;;; 1.2
+(/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5)))))
+   (* 3 (- 6 2) (- 2 7))) ; -37/150
+
+;;; 1.3
+(define (square x) (* x x))
+(define (sum-of-square x y) (+ (square x) (square y)))
+(define (f a b c)
+  (cond ((and (= a b) (= b c)) (sum-of-square a b))
+        ((and (= a b) (< b c)) (sum-of-square b c))
+        ((and (= a b) (< c b)) (sum-of-square a b))
+        ((and (= b c) (< c a)) (sum-of-square c a))
+        ((and (= b c) (< a b)) (sum-of-square b c))
+        ((and (= c a) (< a b)) (sum-of-square a b))
+        ((and (= c a) (< b a)) (sum-of-square a c))
+        ((and (< a b) (< b c)) (sum-of-square b c))
+        ((and (< b c) (< c a)) (sum-of-square c a))
+        ((and (< c a) (< a b)) (sum-of-square a b))))
+
+;;; 1.4
+((define (a-plus-abs-b a b))
+ ((if (> b 0) + -) a b))
+;; (> b 0) の評価結果によって演算子自体を返却する
+
+;;; 1.5
+(define (p) (p))
+(define (test x y)
+  (if (= x 0) 0 y))
+
+(test 0 (p))
+
+;;; 1.6
+(define (improve guess x)
+  (average guess (/ x guess)))
+(define (average x y)
+  (/ (+ x y) 2))
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+(define (sqrt-iter guess x)
+  (new-if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x) x)))
+
+(sqrt-iter 1.0 9) ; not response
+;; if は述語式predicateを最初に評価する特殊形式なので、good-enough?が評価されるが、
+;; new-if はgood-enough?が評価されず無限に (sqrt-iter (improve guess x) x) が呼ばれてしまう
+
+;;; 1.7
+;; good-enough? で想定している許容誤差0.001より小さい数については計算できない
+;; また、非常に大きい数では計算が終わらない
+;; guessがどれだけ変化するかに着目して再実装
+;; (define (improve guess x)
+;;   (average guess (/ x guess)))
+;; (define (average x y)
+;;   (/ (+ x y) 2))
+(define (good-enough2? guess pre-guess)
+  (< (/ (abs (- guess pre-guess)) guess) 0.001))
+
+(define (sqrt-iter2 guess pre-guess x)
+  (if (good-enough2? guess pre-guess)
+      guess
+      (sqrt-iter2 (improve guess x) guess x)))
+
+;;; 1.8
+(define (improve2 guess x)
+  (/ (+ (/ x (* guess guess)) (* 2 guess)) 3))
+
+(define (cube-iter guess pre-guess x)
+  (if (good-enough2? guess pre-guess)
+      guess
+      (cube-iter (improve2 guess x) guess x)))
+
+(define (cube x)
+  (cube-iter 0.001 100.0 x))
+
+(cube 8) ; 2.000000136961482
+
+;;; 1.9
+(define (inc x) (+ x 1))
+(define (dec x) (- x 1))
+
+(define (p1 a b)
+  (if (= a 0) b (inc (p1 (dec a) b))))
+(define (p2 a b)
+  (if (= a 0) b (p2 (dec a) (inc b))))
+
+(p1 4 5)
+;; (if (= 4 0) 5 (inc (p1 (- 4 1) 5)))
+;; (inc (p1 3 5))
+;; (inc (if (= 3 0) 5 (inc (p1 (- 3 1) 5))))
+;; (inc (inc (p1 2 5)))
+;; (inc (inc (if (= 2 0) 5 (inc (p1 (- 2 1) 5)))))
+;; (inc (inc (inc (p1 1 5))))
+;; (inc (inc (inc (if (= 1 0) 5 (inc (p1 (- 1 1) 5))))))
+;; (inc (inc (inc (inc (p1 0 5)))))
+;; (inc (inc (inc (inc (if (= 0 0) 5 (inc (p1 (- 0 1) 5)))))))
+;; (inc (inc (inc (int 5))))
+;; (inc (inc (inc 6)))
+;; (inc (inc 7))
+;; (inc 8)
+;; 9
+(p2 4 5)
+;; (if (= 4 0) 5 (p2 (dec 4) (inc 5)))
+;; (p2 (dec 4) (inc 5))
+;; (p2 3 6)
+;; (if (= 3 0) 6 (p2 (dec 3) (inc 6)))
+;; (p2 (dec 3) (inc 6))
+;; (p2 2 7)
+;; (if (= 2 0) 7 (p2 (dec 2) (inc 7)))
+;; (p2 (dec 2) (inc 7))
+;; (p2 1 8)
+;; (if (= 1 0) 8 (p2 (dec 1) (inc 8)))
+;; (p2 (dec 1) (inc 8))
+;; (p2 0 9)
+;; (if (= 0 0) 9 (p2 (dec 0) (inc 9)))
+;; 9
+
+;;; 1.10
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1) (A x (- y 1))))))
+(A 1 10) ; 1024
+(A 2 4) ; 65536
+(A 3 3) ; 65536
+
+(define (f n) (A 0 n)) ;; => 2n
+(define (g n) (A 1 n)) ;; => 2^n
+(define (h n) (A 2 n)) ;; => 2^2^2^...^2（2のn回のべき乗）
+(define (k n) (* 5 n n))
+
+;;; 1.11
+;; f(n) = n (if n < 3) | f(n-1) + 2f(n-2) + 3f(n-3) (if n >= 3)
+;; recursion
+(define (f1 n)
+  (if (< n 3)
+        n
+        (+ (f1 (- n 1)) (* 2 (f1 (- n 2))) (* 3 (f1 (- n 3))))))
+;; iteration
+;; a <- b
+;; b <- c
+;; c <- f(n-1) + 2f(n-2) + 3f(n-3) = c + 2b + 3a
+(define (f2 n)
+  (f2-iter 0 1 2 n))
+(define (f2-iter a b c count)
+  (if (= count 0)
+      a
+      (f2-iter b c (+ c (* 2 b) (* 3 a)) (- count 1))))
+
+;;; 1.12
+;; Pascal's triangle
+(define (pt h n) ; height number
+  (if (or (= n 1) (= n h))
+      1
+      (+ (pt (- h 1) (- n 1)) (pt (- h 1) n))))
+(pt 1 1) ; 1
+(pt 2 1) ; 1
+(pt 2 2) ; 1
+(pt 3 2) ; 2
+(pt 5 3) ; 6
+
+;;; 1.13 略
+
+;;; 1.14
+;; http://community.schemewiki.org/?sicp-ex-1.14
+
+;;; 1.15
+(define (cube x) (* x x x))
+(define (p x) (- (* 3 x) (* 4 (cube x))))
+(define (sine angle)
+  (if (not (> (abs angle) 0.1))
+      angle
+      (p (sine (/ angle 3.0)))))
+
+;; (a)
+(sine 12.15)
+;; (p (sine 4.05))
+;; (p (p (sine 1.35)))
+;; (p (p (p (sine 0.45))))
+;; (p (p (p (p (sine 0.15)))))
+;; (p (p (p (p (p (sine 0.05))))))
+
+;; (b)
+(sine a)
+;; a / 3.0 ごとにブレイクダウンされていく
+;; -> a が3倍されるごとにステップ数が1増える
+;; -> O(log3(a))
+
+;;; 1.16
+(define (fast-expt2 b n)
+  (fast-expt-iter b n 1))
+(define (even? n)
+  (= (remainder n 2) 0))
+;; b が偶 = a * (b^2)^(n/2)   => a * b^n
+;; b が奇 = (a * b) * b^(n-1) => a * b^n
+(define (fast-expt-iter b n a)
+  (cond ((= n 0) a)
+        ((even? n) (fast-expt-iter (* b b) (/ n 2) a))
+        (else (fast-expt-iter b (- n 1) (* b a)))))
+(fast-expt2 2 5) ; 32
+;; 各状態を通じて一定であるような不変量invariant quantityを定義するのが、反復アルゴリズムを設計する上で一般的かつ強力なテクニック
+
+;;; 1.17
+(define (double x)
+  (* x 2))
+(define (halve x)
+  (/ x 2))
+(define (even? n)
+  (= (remainder n 2) 0))
+;; a*b
+;; b が偶 = (a*2) * (b/2)           => a*b
+;; b が奇 = ((a*2) * ((b-1)/2)) + a => a*b
+(define (fast-multi b n a)
+  (cond ((= n 0) 0)
+        ((even? n) (fast-multi (double a) (halve b)))
+        (else (+ (fast-multi (double a) (halve (- b 1))) a))))
+
+;;; 1.18
+;; 不変量 a*b+n を考える（不変量が思いつかない。。）
+;; b が偶 = ((a*2) * (b/2)) + n         => a*b+n
+;; b が奇 = ((a*2) * ((b-1)/2)) + a + n => a*b+n
+(define (fast-multi-iter a b n)
+  (cond ((= b 0) n)
+        ((even? b) (fast-multi-iter (double a) (halve b) n))
+        (else (+ (fast-multi (double a) (halve (- b 1))) a n))))
+(define (fast-multi2 a b n)
+  (fast-multi-iter a b 0))
+
+;;; 1.19
+;; フィボナッチ数を対数的ステップで計算する
+;; a <- a + b
+;; b <- a
+;; を、以下のように考える
+;; a <- b*q + a*q + a*p
+;; b <- b*p + a*q
+;; Tpq の一回目と二回目の差分を地道に計算して 'p と 'q を検証する
+;; https://www.serendip.ws/archives/381
+(define (fib n)
+  (fib-iter 1 0 0 1 n))
+(define (fib-iter a b p q count)
+  (cond ((= count 0) b)
+        ((even? count)
+         (fib-iter a
+                   b
+                   (+ (* p p) (* q q))   ;; 'p
+                   (+ (* 2 p p) (* q q)) ;; 'q
+                   (/ count 2)))
+        (else (fib-iter (+ (* b q) (* a q) (* a p))
+                        (+ (* b p) (* a q))
+                        p
+                        q
+                        (- count 1)))))
